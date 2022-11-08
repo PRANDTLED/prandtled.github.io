975a2ad963ae33f22bb32114276cdde89f7ad1e6
PRANDTLED <38784540+PRANDTLED@users.noreply.github.com>
2022-11-08 18:36:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#0d6efd";
+tg.MainButton.show();
+
+let item = "";
+let message = `<b>Заявка с сайта</b>\n`;
+
+document.getElementById("tg").addEventListener("submit", function (e) {
+  e.preventDefault();
+  message += `<b>Отправитель: </b> ${this.name.value}\n`;
+  message += `<b>Почта: </b> ${this.email.value}\n`;
+  message += `<b>UserId: </b> ${tg.initDataUnsafe.user.id}`;
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  tg.sendData(message);
+});
