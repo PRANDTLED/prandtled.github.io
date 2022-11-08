111f96b9132993b85f4930053b20f5ee118c3fdd
PRANDTLED <38784540+PRANDTLED@users.noreply.github.com>
2022-11-08 21:50:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ tg.MainButton.color = "#0d6efd";
 tg.MainButton.show();
 
 let item = "";
-let message = `<b>Заявка с сайта</b>\n`;
+let message = `Имя: `;
 
-document.getElementById("tg").addEventListener("submit", function (e) {
-  e.preventDefault();
-  message += `<b>Отправитель: </b> ${this.name.value}\n`;
-  message += `<b>Почта: </b> ${this.email.value}\n`;
-  message += `<b>UserId: </b> ${tg.initDataUnsafe.user.id}`;
-});
+// document.getElementById("tg").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   message += `<b>Отправитель: </b> ${this.name.value}\n`;
+//   message += `<b>Почта: </b> ${this.email.value}\n`;
+//   message += `<b>UserId: </b> ${tg.initDataUnsafe.user.id}`;
+// });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-  name = document.getElementById('tg').this.name.value;
+  const input = document.getElementById("name"); // Извлекаем элемент input
+  message += `${input.value}`;
   tg.sendData(name);
 });
