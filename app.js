189c1ae8ce6e8f069063309706b09a7a0881832d
PRANDTLED <38784540+PRANDTLED@users.noreply.github.com>
2022-11-16 04:46:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#198754";
-tg.MainButton.text = "СОХРАНИТЬ";
+tg.MainButton.text = "Сохранить";
 tg.MainButton.show();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
